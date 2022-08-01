e1b9b3d7aa8993573107f66f94a084af68230d41
NdegwaJulius <juliusndegwa2@gmail.com>
2022-08-01 13:31:38 +0300

    ""

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp( MaterialApp(
-
-  ),);
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purple,
+          centerTitle: true,
+          title: Text("I Am Happy"),
+        ),
+        body: Image(
+          image: AssetImage(''),
+        ),
+      ),
+    ),
+  );
 }
 
