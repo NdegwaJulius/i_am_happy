0974ff48e5b08d315b1d7ff7d5f1f36966119e4b
NdegwaJulius <juliusndegwa2@gmail.com>
2022-08-01 13:54:40 +0300

    Full I Am Happy App

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,27 @@ void main() {
           centerTitle: true,
           title: Text("I Am Happy"),
         ),
-        body: Image(
-          image: AssetImage(''),
+        body: Center(
+          child: Image(
+            image: AssetImage('images/business-3d-young-man-standing-with-laptop.png'),
+          ),
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(
+        icon: Icon(Icons.home),
+        label: 'Home',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.business),
+        label: 'Business',
+      ),
+      BottomNavigationBarItem(
+        icon: Icon(Icons.school),
+        label: 'School',
+      ),
+      ],
+
         ),
       ),
     ),
